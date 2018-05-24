563dc4c9c83f2e24e61ecc81a794db810662887f
jstefaniuk <jstefaniuk@D2L.com>
2018-05-23 22:21:20 -0500

    try another thing

--- test/acceptance/galen.test.js
+++ test/acceptance/galen.test.js
@@ -79,6 +79,9 @@ polymerTests(browsers, function(test) {
 		rtl && tags.push('rtl') || tags.push('ltr');
 		open && tags.push('open') || tags.push('closed');
 		mobile && tags.push('mobile') || tags.push('desktop');
+		if (!mainline) {
+			tags.push('no-d2l-shadow');
+		}
 
 		var cb;
 		if (open) {
